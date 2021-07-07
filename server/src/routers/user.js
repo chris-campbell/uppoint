08f40b9457d6b4e4c31c365d613d92c350f0def7
christopher campbell <hopprdev@gmail.com>
2021-07-07 12:12:58 -0400

    Add routes to both log out a single user token or multiple

--- server/src/routers/user.js
+++ server/src/routers/user.js
@@ -36,6 +36,31 @@ router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
+// [POST] Log user out
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+
+    await req.user.save();
+    res.send("User Logged out!");
+  } catch (error) {
+    res.status(500);
+  }
+});
+
+// [POST] Logout all
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send("All users logged out");
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
 // [GET] Retrieve single user from DB
 router.get("/users/:id", async (req, res) => {
   const user = await User.findById(req.params.id);
