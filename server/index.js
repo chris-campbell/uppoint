839b9322299bb1288305f42d6fe942a4a90e20aa
christopher campbell <hopprdev@gmail.com>
2021-07-08 12:00:35 -0400

    Remove unecessary files from client src and add proxy to server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(userRouter);
 
 // PORT DECLARATION
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log("Connection success: ", `Server is now running on port ${PORT}`);
--- server/src/routers/user.js
+++ server/src/routers/user.js
@@ -3,127 +3,131 @@ import User from "../models/user.js";
 import auth from "../middleware/auth.js";
 const router = express.Router();
 
-router.post("/users/login", async (req, res) => {
-  try {
-    const user = await User.findByCredentials(
-      req.body.email,
-      req.body.password
-    );
-    const token = await user.generateAuthToken();
-
-    res.send({ user, token });
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+router.get("/api", (req, res) => {
+  res.status(404).json({ message: "Hello from server!" });
 });
 
-// [POST] Create single user in DB
-router.post("/users", async (req, res) => {
-  const user = User(req.body);
-  try {
-    await user.save();
+// router.post("/users/login", async (req, res) => {
+//   try {
+//     const user = await User.findByCredentials(
+//       req.body.email,
+//       req.body.password
+//     );
+//     const token = await user.generateAuthToken();
 
-    const token = await user.generateAuthToken();
+//     res.send({ user, token });
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// });
 
-    res.status(201).send({ user, token });
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
+// // [POST] Create single user in DB
+// router.post("/users", async (req, res) => {
+//   const user = User(req.body);
+//   try {
+//     await user.save();
 
-// [GET] Retrieve user profile
-router.get("/users/me", auth, async (req, res) => {
-  res.send(req.user);
-});
+//     const token = await user.generateAuthToken();
 
-// [POST] Log user out
-router.post("/users/logout", auth, async (req, res) => {
-  try {
-    req.user.tokens = req.user.tokens.filter((token) => {
-      return token.token !== req.token;
-    });
+//     res.status(201).send({ user, token });
+//   } catch (error) {
+//     res.status(400).send(error.message);
+//   }
+// });
 
-    await req.user.save();
-    res.send("User Logged out!");
-  } catch (error) {
-    res.status(500);
-  }
-});
+// // [GET] Retrieve user profile
+// router.get("/users/me", auth, async (req, res) => {
+//   res.send(req.user);
+// });
 
-// [POST] Logout all
-router.post("/users/logoutAll", auth, async (req, res) => {
-  try {
-    req.user.tokens = [];
-    await req.user.save();
-    res.send("All users logged out");
-  } catch (error) {
-    res.status(500).send("Something went wrong");
-  }
-});
+// // [POST] Log user out
+// router.post("/users/logout", auth, async (req, res) => {
+//   try {
+//     req.user.tokens = req.user.tokens.filter((token) => {
+//       return token.token !== req.token;
+//     });
 
-// [GET] Retrieve single user from DB
-router.get("/users/:id", async (req, res) => {
-  const user = await User.findById(req.params.id);
+//     await req.user.save();
+//     res.send("User Logged out!");
+//   } catch (error) {
+//     res.status(500);
+//   }
+// });
 
-  if (!user) {
-    return res.status(404).send();
-  }
+// // [POST] Logout all
+// router.post("/users/logoutAll", auth, async (req, res) => {
+//   try {
+//     req.user.tokens = [];
+//     await req.user.save();
+//     res.send("All users logged out");
+//   } catch (error) {
+//     res.status(500).send("Something went wrong");
+//   }
+// });
 
-  res.send(user);
-});
+// // [GET] Retrieve single user from DB
+// router.get("/users/:id", async (req, res) => {
+//   const user = await User.findById(req.params.id);
 
-// [PATCH] Update single user in the DB
-router.patch("/users/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowUpdates = [
-    "firstName",
-    "lastName",
-    "email",
-    "gender",
-    "dateOfBirth",
-    "phone",
-    "location",
-  ];
+//   if (!user) {
+//     return res.status(404).send();
+//   }
 
-  const isValidOperation = updates.every((update) =>
-    allowUpdates.includes(update)
-  );
+//   res.send(user);
+// });
 
-  if (!isValidOperation) {
-    return res.status(400).send({ error: "Invalid update" });
-  }
+// // [PATCH] Update single user in the DB
+// router.patch("/users/:id", async (req, res) => {
+//   const updates = Object.keys(req.body);
+//   const allowUpdates = [
+//     "firstName",
+//     "lastName",
+//     "email",
+//     "gender",
+//     "dateOfBirth",
+//     "phone",
+//     "location",
+//   ];
 
-  try {
-    const user = await User.findById(req.params.id);
+//   const isValidOperation = updates.every((update) =>
+//     allowUpdates.includes(update)
+//   );
 
-    updates.forEach((update) => {
-      user[update] = req.body[update];
-    });
+//   if (!isValidOperation) {
+//     return res.status(400).send({ error: "Invalid update" });
+//   }
 
-    await user.save();
+//   try {
+//     const user = await User.findById(req.params.id);
 
-    if (!user) {
-      return res.status(404).send();
-    }
+//     updates.forEach((update) => {
+//       user[update] = req.body[update];
+//     });
 
-    res.send(user);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-});
+//     await user.save();
 
-// [DELETE] Delete single user from DB
-router.delete("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
+//     if (!user) {
+//       return res.status(404).send();
+//     }
 
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
+//     res.send(user);
+//   } catch (error) {
+//     res.status(400).send(error.message);
+//   }
+// });
+
+// // [DELETE] Delete single user from DB
+// router.delete("/users/:id", async (req, res) => {
+//   try {
+//     const user = await User.findByIdAndDelete(req.params.id);
+//     if (!user) {
+//       return res.status(404).send();
+//     }
+
+//     res.send(user);
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+// });
 
 export default router;
